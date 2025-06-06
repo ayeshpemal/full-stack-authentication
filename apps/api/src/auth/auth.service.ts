c9091d0be361e15758d3d22ce259dfca37dec295
Ayesha pemal <ayeshapemal@gmail.com>
2025-06-06 13:17:30 +0530

    sign up API created

--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -1,4 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private readonly userService: UserService) {}
+    async registerUser(createUserDto: CreateUserDto) {
+        const user = await this.userService.finByEmail(createUserDto.email);
+        if (user) throw new ConflictException('User already exists with this email');
+        return this.userService.create(createUserDto);
+    }
+}
